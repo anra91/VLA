f4e16b55aff2f79ce29703de722072bbdb41f9c0
Andres Ramirez Restrepo <andresramirezrestrepo@andress-macbook-pro.local>
2015-12-05 01:52:23 -0500

    Commit

--- VLA/ViewController.swift
+++ VLA/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Este es un comentario.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
